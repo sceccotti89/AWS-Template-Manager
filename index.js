e9a366e6aa49556a83eba54b5b78cacc14f6ec84
sceccotti89 <stefanoceccotti89@gmail.com>
2019-08-31 12:11:06 +0100

    Added bootstrap

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const { app, BrowserWindow } = require('electron')
 
 // Mantiene un riferimento globale all'oggetto window, se non lo fai, la finestra sarà
 // chiusa automaticamente quando l'oggetto JavaScript sarà garbage collected.
-let win
+let win;
 
 function createWindow () {
   // Creazione della finestra del browser.
@@ -15,41 +15,41 @@ function createWindow () {
   })
 
   // and load the index.html of the app.
-  win.loadFile('index.html')
+  win.loadFile('index.html');
 
   // Apre il Pannello degli Strumenti di Sviluppo.
-  win.webContents.openDevTools()
+  // win.webContents.openDevTools();
 
   // Emesso quando la finestra viene chiusa.
   win.on('closed', () => {
     // Eliminiamo il riferimento dell'oggetto window;  solitamente si tiene traccia delle finestre
     // in array se l'applicazione supporta più finestre, questo è il momento in cui 
     // si dovrebbe eliminare l'elemento corrispondente.
-    win = null
+    win = null;
   })
 }
 
 // Questo metodo viene chiamato quando Electron ha finito
 // l'inizializzazione ed è pronto a creare le finestre browser.
 // Alcune API possono essere utilizzate solo dopo che si verifica questo evento.
-app.on('ready', createWindow)
+app.on('ready', createWindow);
 
 // Terminiamo l'App quando tutte le finestre vengono chiuse.
 app.on('window-all-closed', () => {
   // Su macOS è comune che l'applicazione e la barra menù 
   // restano attive finché l'utente non esce espressamente tramite i tasti Cmd + Q
   if (process.platform !== 'darwin') {
-    app.quit()
+    app.quit();
   }
-})
+});
 
 app.on('activate', () => {
   // Su macOS è comune ri-creare la finestra dell'app quando
   // viene cliccata l'icona sul dock e non ci sono altre finestre aperte.
   if (win === null) {
-    createWindow()
+    createWindow();
   }
-})
+});
 
 // in questo file possiamo includere il codice specifico necessario 
 // alla nostra app. Si può anche mettere il codice in file separati e richiederlo qui.
