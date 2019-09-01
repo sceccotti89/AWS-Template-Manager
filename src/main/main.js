be80da35b1059b92fe85478d1d5a7866c9a827d7
sceccotti89 <stefanoceccotti89@gmail.com>
2019-09-01 11:51:56 +0100

    Store open files into storage account

--- src/main/main.js
+++ src/main/main.js
@@ -1,18 +1,53 @@
 const { ipcRenderer } = require('electron');
+const STORAGE_FILES = "files";
 
+// Read 'openFile' events when new files are selected.
 ipcRenderer.on('openFile', (event, message) => {
-    console.log("Message:", message);
+    const files = message.filePaths;
+    if (typeof(Storage) !== "undefined") {
+        const storage = window.localStorage;
+        const openFiles = JSON.parse(storage.getItem(STORAGE_FILES)) || [];
+
+        files.forEach((filePath) => {
+            if (!openFiles.some((file) => file === filePath)) {
+                openFiles.push(filePath);
+                openFile(filePath);
+            }
+        });
+
+        storage.setItem(STORAGE_FILES, JSON.stringify(openFiles));
+    }
 });
 
 const loadOpenFiles = () => {
     if (typeof(Storage) !== "undefined") {
         const storage = window.localStorage;
-        const openFiles = JSON.parse(storage.getItem("files"));
-        if (openFiles) {
-            // TODO show as many tabs as open files
-            
+        const openFiles = JSON.parse(storage.getItem("files")) || [];
+        openFiles.forEach((filePath) => openFile(filePath));
+    }
+}
+
+const openFile = (filePath) => {
+    const xhr = new XMLHttpRequest();
+    xhr.onloadend = (event) => {
+        if (event.loaded && xhr.response) {
+            const contents = xhr.responseText;
+            displayContents(contents);
+        } else {
+            const storage = window.localStorage;
+            const files = JSON.parse(storage.getItem(STORAGE_FILES));
+            files.splice(files.findIndex((file) => file === filePath), 1);
+            storage.setItem(STORAGE_FILES, JSON.stringify(files));
         }
     }
+    xhr.open('GET', filePath);
+    xhr.send();
+}
+
+function displayContents(contents) {
+    // TODO show display content
+    // const element = document.getElementById('file-content');
+    // element.textContent = contents;
 }
 
 window.onload = loadOpenFiles();
